bf4e8526a3e715a1a6246a08acd66214757badde
Orion-Technologies <abel.archila@gmail.com>
2021-08-29 14:51:49 -0600

    chaging order of toUpperCase

--- testing.js
+++ testing.js
@@ -4,11 +4,10 @@ function greet(name) {
        return "Hello, " + name; 
     } else if (name === null) {
         return "Hello there!";
-    } else if (name === name.toUpperCase()) {
-        return "HELLO " + name.toUpperCase() + "!";
-     } else if (name.isArray()) {
-        name = ['Jose', 'Pep'];
-        let nameF = [""]
-        }
+    } else if (typeof name === 'object') {
+        return "Hello, " + name;
+        } else if (name === name.toUpperCase()) {
+        return "HELLO " + name + "!";
      }
+}
      
